06fa603979b3bc3baf04bfe156ab8a2cd8b94cf8
triantisna <triantisna17@gmail.com>
2026-09-02 00:22:37 +0700

    Update api/index.php for direct Laravel bootstrap

--- api/index.php
+++ api/index.php
@@ -1,7 +1,25 @@
 <?php
 
-// Tentukan root path project Laravel
-$_ENV['APP_BASE_PATH'] = dirname(__DIR__);
+// Tentukan base path
+define('LARAVEL_START', microtime(true));
 
-// Forward request ke public/index.php bawaan Laravel
-require_once __DIR__ . '/../public/index.php';
+// Maintenance mode check
+if (file_exists($maintenance = __DIR__ . '/../storage/framework/maintenance.php')) {
+    require $maintenance;
+}
+
+// Register Composer autoloader
+require __DIR__ . '/../vendor/autoload.php';
+
+// Bootstrap Laravel dan tangani request
+$app = require_once __DIR__ . '/../bootstrap/app.php';
+
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+
+$response = $kernel->handle(
+    $request = Illuminate\Http\Request::capture()
+);
+
+$response->send();
+
+$kernel->terminate($request, $response);
