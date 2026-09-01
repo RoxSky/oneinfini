9c57f03368e6dc85856f504b1ab414efc33c3ddf
triantisna <triantisna17@gmail.com>
2026-09-02 03:42:40 +0700

    Redirect all Laravel bootstrap caches to writable /tmp directory

--- api/index.php
+++ api/index.php
@@ -1,18 +1,40 @@
 <?php
 
-// Tangkap semua error agar tidak melempar BindingResolutionException view
 error_reporting(E_ALL);
 ini_set('display_errors', '1');
 
 define('LARAVEL_START', microtime(true));
 
-// Pastikan direktori /tmp untuk storage aman
-$tmpStorage = '/tmp/storage';
-foreach (['app', 'framework/cache', 'framework/sessions', 'framework/views', 'logs'] as $dir) {
-    if (!is_dir($tmpStorage . '/' . $dir)) {
-        mkdir($tmpStorage . '/' . $dir, 0755, true);
+// Siapkan folder /tmp yang memiliki akses tulis di Vercel
+$tmpDir = '/tmp/storage';
+$directories = [
+    'app',
+    'framework/cache',
+    'framework/sessions',
+    'framework/views',
+    'logs',
+    'bootstrap/cache'
+];
+
+foreach ($directories as $dir) {
+    if (!is_dir($tmpDir . '/' . $dir)) {
+        mkdir($tmpDir . '/' . $dir, 0755, true);
     }
 }
+
+// Paksa Laravel menggunakan /tmp untuk semua file cache sistem
+$_ENV['APP_SERVICES_CACHE'] = $tmpDir . '/bootstrap/cache/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = $tmpDir . '/bootstrap/cache/packages.php';
+$_ENV['APP_CONFIG_CACHE'] = $tmpDir . '/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE'] = $tmpDir . '/bootstrap/cache/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = $tmpDir . '/bootstrap/cache/events.php';
+$_ENV['VIEW_COMPILED_PATH'] = $tmpDir . '/framework/views';
+
+$_SERVER['APP_SERVICES_CACHE'] = $_ENV['APP_SERVICES_CACHE'];
+$_SERVER['APP_PACKAGES_CACHE'] = $_ENV['APP_PACKAGES_CACHE'];
+$_SERVER['APP_CONFIG_CACHE'] = $_ENV['APP_CONFIG_CACHE'];
+$_SERVER['APP_ROUTES_CACHE'] = $_ENV['APP_ROUTES_CACHE'];
+$_SERVER['APP_EVENTS_CACHE'] = $_ENV['APP_EVENTS_CACHE'];
 
 // Muat Composer Autoloader
 require __DIR__ . '/../vendor/autoload.php';
