a7380fde22ee5ff8c385c1ef0464b22ad5473caa
triantisna <triantisna17@gmail.com>
2026-09-02 00:15:09 +0700

    Fix serverless entry point and base path for Vercel

--- api/index.php
+++ api/index.php
@@ -1,4 +1,7 @@
 <?php
 
-// Forward request langsung ke public/index.php bawaan Laravel
-require __DIR__ . '/../public/index.php';
+// Tentukan root path project Laravel
+$_ENV['APP_BASE_PATH'] = dirname(__DIR__);
+
+// Forward request ke public/index.php bawaan Laravel
+require_once __DIR__ . '/../public/index.php';
